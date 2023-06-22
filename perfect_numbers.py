e0ebfc19bb37677303b9ed8034f016ff42828259
anedelev <anedelev@typedepot.com>
2023-06-22 17:03:49 +0300

    chore: added docstring explanation

--- perfect_numbers.py
+++ perfect_numbers.py
@@ -1,4 +1,24 @@
+'''
+Perfect Number
+
+A perfect number is a positive integer that is equal to the sum of its proper positive divisors, 
+that is, the sum of its positive divisors excluding the number itself (also known as its aliquot sum). 
+Equivalently, a perfect number is a number that is half the sum of all its positive divisors (including itself). 
+Write a method that prints the first 4 perfect numbers.
+
+Perfect Number Example
+
+6 = 1 + 2 + 3
+
+'''
+
+
 from math import sqrt, ceil
+
+
+'''
+What's important here is that you can find the divisors of an integer by only traversing to the sqrt of this number!!! 
+'''
 
 def find_divisors(n):
     divisors = 0
