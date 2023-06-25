52dfbd827500c4efa0fefbff8466fd8f78c28e57
anedelev <anedelev@typedepot.com>
2023-06-26 00:34:57 +0300

    exercise: postfix sum and product

--- concepts/prefix_postfix.py
+++ concepts/prefix_postfix.py
@@ -11,7 +11,6 @@ print(prefix_sum)
 
 
 # prefix product
-nums = [1,2,3,4,5]
 
 prefix_product = []
 current_product = 1
@@ -20,3 +19,23 @@ for num in nums:
     prefix_product.append(current_product)
 
 print(prefix_product)
+
+# postfix sum 
+
+postfix_sum = []
+current_sum = 0
+for num in nums[::-1]:
+    current_sum += num
+    postfix_sum.insert(0,current_sum)
+
+print(postfix_sum)
+
+# postfix product 
+
+postfix_product = []
+current_sum = 1
+for num in nums[::-1]:
+    current_sum *= num
+    postfix_product.insert(0,current_sum)
+
+print(postfix_product)
